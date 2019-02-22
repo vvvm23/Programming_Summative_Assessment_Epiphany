851d12f64144d2c88b37dd90d2ae6d9aa21dc9b0
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2019-02-22 15:55:13 +0000

    fixed fly in

--- client/index.js
+++ client/index.js
@@ -1,7 +1,16 @@
+
+$(document).ready(function() {
+    $(".fullmain").transition({animation: 'fly left',
+                                duration: 0});
+});
+
 $(document).ready(function () {
     $(".accordion").accordion();
 });
 
 document.onkeypress = function() {
     $(".emptymain").transition('fly left');
+    $(".fullmain").transition('fly left');
+    document.getElementById("empty").style = "position: absolute";
+    document.getElementById("full").style = "position: absolute";
 };
