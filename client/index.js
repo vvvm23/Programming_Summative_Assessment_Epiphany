28cbb54e854ad8a6db08ee71fa89046dd6c9a64e
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2019-02-25 21:34:51 +0000

    added basic menu functionality, not sure what role submit button will play

--- client/index.js
+++ client/index.js
@@ -1,16 +1,84 @@
+var a = 0;
+var b = 0;
 
 $(document).ready(function() {
-    $(".fullmain").transition({animation: 'fly left',
-                                duration: 0});
+    $(".stats").transition({animation: 'fly left',
+    duration: 0});
+    $(".map").transition({animation: 'fly left',
+    duration: 0});
+    $(".wiki").transition({animation: 'fly left',
+    duration: 0});
 });
 
 $(document).ready(function () {
     $(".accordion").accordion();
-});
 
-document.onkeypress = function() {
-    $(".emptymain").transition('fly left');
-    $(".fullmain").transition('fly left');
-    document.getElementById("empty").style = "position: absolute";
-    document.getElementById("full").style = "position: absolute";
-};
+    document.getElementById('menu_one').onclick = function() {
+        b = 1;
+        if (a != b) {
+            if (a==0) {
+                // Coming from start
+                $('.emptymain').transition('fly left');
+                $('.stats').transition('fly left');
+            } else if (a==1) {
+                // Coming from stats
+                // pass
+            } else if (a==2) {
+                // Coming from map
+                $('.map').transition('fly left');
+                $('.stats').transition('fly left');
+            } else if (a==3) {
+                // Coming from wikipedia
+                $('.wiki').transition('fly left');
+                $('.stats').transition('fly left');
+            }
+            a = 1
+        }
+    };
+    
+    document.getElementById('menu_two').onclick = function() {
+        b = 2;
+        if (a != b) {
+            if (a==0) {
+                // Coming from start
+                $('.emptymain').transition('fly left');
+                $('.map').transition('fly left');
+            } else if (a==1) {
+                // Coming from stats
+                $('.stats').transition('fly left');
+                $('.map').transition('fly left');
+            } else if (a==2) {
+                // Coming from map
+                // pass
+            } else if (a==3) {
+                // Coming from wikipedia
+                $('.wiki').transition('fly left');
+                $('.map').transition('fly left');
+            }
+            a = 2
+        }
+    };
+    
+    document.getElementById('menu_three').onclick = function() {
+        b = 3;
+        if (a != b) {
+            if (a==0) {
+                // Coming from start
+                $('.emptymain').transition('fly left');
+                $('.wiki').transition('fly left');
+            } else if (a==1) {
+                // Coming from stats
+                $('.stats').transition('fly left');
+                $('.wiki').transition('fly left');
+            } else if (a==2) {
+                // Coming from map
+                $('.map').transition('fly left');
+                $('.wiki').transition('fly left');
+            } else if (a==3) {
+                // Coming from wikipedia
+                // pass
+            }
+            a = 3
+        }
+    };
+});
