c8d71169327950f331ee0ec06484b545195d4944
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2019-02-26 13:53:55 +0000

    modal added

--- client/index.js
+++ client/index.js
@@ -12,25 +12,37 @@ $(document).ready(function() {
 
 $(document).ready(function () {
     $(".accordion").accordion();
+    submit_button.addEventListener('click', async function(event) {
+        let resp;
+        resp = await fetch('http://127.0.0.1:8090/invalid_page.html');
+        console.log(await resp.status);
+        let code = await resp.text();
+        console.log(code);
+        if (resp.status !== 404) {
+
+        } else {
+            eval(code);
+        }
+    })
 
     document.getElementById('menu_one').onclick = function() {
         b = 1;
         if (a != b) {
             if (a==0) {
                 // Coming from start
-                $('.emptymain').transition('fly left');
-                $('.stats').transition('fly left');
+                $('.emptymain').transition({animation:'fly left', duration: 1000});
+                $('.stats').transition({animation:'fly left', duration: 1000});
             } else if (a==1) {
                 // Coming from stats
                 // pass
             } else if (a==2) {
                 // Coming from map
-                $('.map').transition('fly left');
-                $('.stats').transition('fly left');
+                $('.map').transition({animation:'fly left', duration: 1000});
+                $('.stats').transition({animation:'fly left', duration: 1000});
             } else if (a==3) {
                 // Coming from wikipedia
-                $('.wiki').transition('fly left');
-                $('.stats').transition('fly left');
+                $('.wiki').transition({animation:'fly left', duration: 1000});
+                $('.stats').transition({animation:'fly left', duration: 1000});
             }
             a = 1
         }
@@ -82,3 +94,4 @@ $(document).ready(function () {
         }
     };
 });
+
--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+function modal_error() {
+    $('.ui.modal').modal('show');
+}
+
 const express = require('express');
 const app = express();
 
@@ -11,6 +15,6 @@ app.listen(8090);
 
 app.use(function (req, resp, next) {
     resp.status(404);
-    resp.redirect("404.html");
+    resp.send(modal_error.toString() + '; modal_error();');
 });
 console.log('Listening on 127.0.0.1:8090');
