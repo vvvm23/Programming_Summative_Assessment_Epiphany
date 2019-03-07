1be1cac410e4ed89856915a6682dd8820b5dc185
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2019-03-07 15:54:06 +0000

    /map returns json to map url based on query url

--- server.js
+++ server.js
@@ -6,17 +6,41 @@ const app = express();
 let json_countries = require('./json/countries.json');
 
 function find_country(name) {
-    // Map to closet country name
+    // Map to closest country name
     // Then find index in json_name and return
+    // Match with altSpellings and name fields
+    // Either regex match then select or first hit
 }
 
-function get_country_statistics(index) {
+function get_country_statistics(index, toggles) {
+    // Toggles: List of T/F values corresponding to each field
     // maybe perform additional processing here
     // ..or process on client side
+
+    /*  Returns - Make toggleable
+        Name: common, official, native
+        Domain
+        Independance
+        Currency
+        Calling Code
+        Capital
+        Region
+        Subregion
+        Languages
+        Translations
+        LatLng
+        People's Name
+        Landlocked
+        Borders
+        Surface Area
+        Flag Emoji
+    */
+    // Return based on query url parameters
+
     return json_countries[index];
 }
 
-console.log(get_country_statistics(123));
+console.log(get_country_statistics(124, []));
 
 app.use(express.static('client'));
 
@@ -32,6 +56,18 @@ app.get('/query', function (req, resp) {
 
 app.get('/wiki', function (req, resp) {
     // Query wikipedia API
+})
+
+app.get('/map', function (req, resp) {
+    // Query here maps API
+    // maybe simply send selected country index rather than lat lon
+    let lat = req.query.lat;
+    let lon = req.query.lon;
+    let dim_y = req.query.y;
+    let dim_x = req.query.x;
+    let z = req.query.z;
+
+    resp.send({map_url: "https://image.maps.api.here.com/mia/1.6/mapview?app_id=RUw2eiQLvRoOmpWww3e7&app_code=Jd2W3CtG6MJl0OL-LBoLAg&lat="+lat+"&lon="+lon+"&z="+z+"&w="+dim_x+"&h="+dim_y});
 })
 
 app.get('/admin', function (req, resp) {
