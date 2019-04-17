6c132c813ad3ec203ab544a245c6594d3b598900
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2019-04-17 12:50:59 +0100

    basic transitions on admin page

---
+++ client/admin.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+    $('.add').transition({animation: 'fly left', duration: 0});
+    $('.delete').transition({animation: 'fly left', duration: 0});
+
+    a = 2;
+    b = 2;
+    document.getElementById('menu_one').onclick = function() {
+        b = 1;
+        if (a != b) {
+            if (a==1) {
+                // Coming from add
+                // pass
+            } else if (a==2) {
+                // Coming from edit
+                $('.edit').transition({animation:'fly left', duration: 1000});
+                $('.add').transition({animation:'fly left', duration: 1000});
+            } else if (a==3) {
+                // Coming from delete
+                $('.delete').transition({animation:'fly left', duration: 1000});
+                $('.add').transition({animation:'fly left', duration: 1000});
+            }
+            a = 1
+        }
+    };
+    
+    document.getElementById('menu_two').onclick = function() {
+        b = 2;
+        if (a != b) {
+            if (a==1) {
+                // Coming from add
+                $('.add').transition('fly left');
+                $('.edit').transition('fly left');
+            } else if (a==2) {
+                // Coming from edit
+                // pass
+            } else if (a==3) {
+                // Coming from delete
+                $('.delete').transition('fly left');
+                $('.edit').transition('fly left');
+            }
+            a = 2
+        }
+    };
+    
+    document.getElementById('menu_three').onclick = function() {
+        b = 3;
+        if (a != b) {
+            if (a==1) {
+                // Coming from add
+                $('.add').transition('fly left');
+                $('.delete').transition('fly left');
+            } else if (a==2) {
+                // Coming from edit
+                $('.edit').transition('fly left');
+                $('.delete').transition('fly left');
+            } else if (a==3) {
+                // Coming from delete
+                // pass
+            }
+            a = 3
+        }
+    };
+})
